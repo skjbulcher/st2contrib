90f8e7fa9638a603381836e94d3ea32e4b844004
John Bulcher <jbulcher@starkartthenala.com>
2016-10-17 11:16:48 -0700

    add delete_webhook action

---
+++ packs/trello/actions/delete_webhook.py
@@ -0,0 +1,19 @@
+from lib import action
+from pprint import pprint
+
+class DeleteWebhookAction(action.BaseAction):
+    def run(self, webhook_id, api_key=None, token=None):
+        if api_key:
+            self._set_creds(api_key=api_key, token=token)
+
+        hooks = self._client().list_hooks(token)
+
+        if hooks:
+            for hook in hooks:
+                if hook.id == webhook_id:
+                    hook.delete()
+                    return webhook_id
+        else:
+            return (False, "Trello did not return any hooks")
+
+        return (False, "hook id {} does not exist".format(webhook_id))
